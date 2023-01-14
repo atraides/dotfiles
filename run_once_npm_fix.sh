062509ea98b7825fbffc737d7eb504c0cfa1ae58
Dániel Hagyárossy <daniel@hagyarossy.hu>
2023-01-14 20:11:12 +0100

    add: helper script to fix the npm installation

---
+++ run_once_npm_fix.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ${HOME}/.tools/npm/bin
+mkdir -p ${HOME}/.tools/lib/node_modules/corepack/dist/
+
+touch ${HOME}/.tools/npm/bin/corepack
+touch ${HOME}/.tools/lib/node_modules/corepack/dist/corepack.js
